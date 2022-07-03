257ea01ee09725e5db879bd7bfcb4afb9859d9ef
Curwen Arthurs <curwenarthurs@gmail.com>
2022-07-02 20:29:20 -0600

    Completed raw version of amortization schedule
    Includes additional HTML input fields for calculation of loan amortization schedule. Finished the javascript to render the table.

--- main.js
+++ main.js
@@ -4,33 +4,61 @@
 document.querySelector(".btn-link").addEventListener("click", displayTable);
 
 function displayTable(){
-    let originalValue = document.querySelector(".value").value;
-    let currentValue = originalValue;
+    let loanAmount = document.querySelector(".amount").value;
+    let term = document.querySelector(".term").value * 12;
+    let interestRate = document.querySelector(".rate").value / 100;
+    let monthlyInterestRate = interestRate / 12;
+
+    let paymentAmount = (loanAmount * monthlyInterestRate) / (1 - Math.pow((1 + monthlyInterestRate), (-1 * term)));
+
+    let principal = 0;
+    let monthlyInterest = 0;
+    let totalInterest = 0;
+    let balance = loanAmount;
+
     let placeholder = document.querySelector(".data-output");
     let out = "";
-    
-    for (i = 0; i < 5; i++) {
+
+    for (i = 0; i < term; i++) {
         if (i == 0) {
+            principal = paymentAmount - (loanAmount * monthlyInterestRate);
+            monthlyInterest = loanAmount * monthlyInterestRate;
+            totalInterest += monthlyInterest;
+            balance = balance - principal; 
+
             out += `
             <tr>
                 <td>${i + 1}</td>
-                <td>${originalValue}</td>
-                <td>20%</td>
-                <td>${currentValue}</td>
+                <td>${paymentAmount.toFixed(2)}</td>
+                <td>${principal.toFixed(2)}</td>
+                <td>${monthlyInterest.toFixed(2)}</td>
+                <td>${totalInterest.toFixed(2)}</td>
+                <td>${balance.toFixed(2)}</td>
             </tr>
             `;
         }
         else {
+            principal = paymentAmount - (balance * monthlyInterestRate);
+            monthlyInterest = balance * monthlyInterestRate;
+            totalInterest += monthlyInterest;
+            balance = balance - principal; 
+
+            if (i == term - 1) {
+                balance = Math.abs(balance);
+            }
+
             out += `
             <tr>
-                <td>${i + 1}</td>
-                <td>${originalValue}</td>
-                <td>20%</td>
-                <td>${currentValue -= currentValue * 0.2}</td>
+            <td>${i + 1}</td>
+            <td>${paymentAmount.toFixed(2)}</td>
+            <td>${principal.toFixed(2)}</td>
+            <td>${monthlyInterest.toFixed(2)}</td>
+            <td>${totalInterest.toFixed(2)}</td>
+            <td>${balance.toFixed(2)}</td>
             </tr>
             `;
-        }
-    
+        }   
+    }    
         placeholder.innerHTML = out;
-    }
 }
+
